703219accd17ae5a3ed5825a7ed298fa7bed69ac
wukelly <kelly.wu.98@gmail.com>
2019-05-21 03:19:33 -0400

    adding judge model

--- judging/admin.py
+++ judging/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Judge
+
+admin.site.register(Judge)
--- judging/models.py
+++ judging/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Judge(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organization = models.CharField(max_length=50)
+    job_title = models.CharField(max_length=50)
