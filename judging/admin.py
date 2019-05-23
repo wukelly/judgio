56f5f9defaca9dad719e15e0d7e8bd1cf11b548e
wukelly <kelly.wu.98@gmail.com>
2019-05-22 21:07:00 -0700

    adding sponsor and check-in status to judge model

--- judging/admin.py
+++ judging/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+import csv, sys, os
 
 from .models import Judge
 
-admin.site.register(Judge)
+# controls /admin/judging/display
+class JudgeAdmin(admin.ModelAdmin):
+    # Table columns
+    list_display = ('name', 'organization', 'job_title', 'email', 'sponsor_judge', 'checked_in')
+    search_fields = ['user__first_name', 'user__last_name', 'organization', 'user__email']
+
+# add import from csv option
+
+
+admin.site.register(Judge, JudgeAdmin)
--- judging/models.py
+++ judging/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Add organization and job title atributes to users who are Judges
 class Judge(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organization = models.CharField(max_length=50)
     job_title = models.CharField(max_length=50)
+    sponsor_judge = models.BooleanField(default=False)
+    checked_in = models.BooleanField(default=False)
+
+    def name(self):
+        return self.user.first_name + ' ' + self.user.last_name
+
+    def email(self):
+        return self.user.email
+
+# make migration commands
+# python manage.py makemigrations judging
+# python manage.py sqlmigrate judging 0001
+# python manage.py migrate
