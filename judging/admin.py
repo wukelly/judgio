026050072e47d09a2799400c5183f5b10f007c26
wukelly <kelly.wu.98@gmail.com>
2019-05-26 22:54:05 -0700

    adding name to judge model and modifying form fields

--- judging/admin.py
+++ judging/admin.py
@@ -5,6 +5,16 @@ from .models import Judge
 
 # controls /admin/judging/display
 class JudgeAdmin(admin.ModelAdmin):
+    # editing judge form fields
+    fieldsets = [
+        ('General Information',     {'fields': ['first_name',
+                                                'last_name',
+                                                'organization',
+                                                'job_title',
+                                                'sponsor_judge']}),
+        ('Event Day',               {'fields': ['checked_in']}),
+    ]
+
     # Table columns
     list_display = ('name', 'organization', 'job_title', 'email', 'sponsor_judge', 'checked_in')
     search_fields = ['user__first_name', 'user__last_name', 'organization', 'user__email']
--- judging/models.py
+++ judging/models.py
@@ -4,13 +4,15 @@ from django.contrib.auth.models import User
 # Add organization and job title atributes to users who are Judges
 class Judge(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     organization = models.CharField(max_length=50)
     job_title = models.CharField(max_length=50)
     sponsor_judge = models.BooleanField(default=False)
     checked_in = models.BooleanField(default=False)
 
     def name(self):
-        return self.user.first_name + ' ' + self.user.last_name
+        return self.first_name + ' ' + self.last_name
 
     def email(self):
         return self.user.email
