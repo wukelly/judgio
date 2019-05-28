05a6f50af7385fda38a2b361030109dc3cd612fd
wukelly <kelly.wu.98@gmail.com>
2019-05-27 22:34:11 -0700

    fix typo

--- judgio/urls.py
+++ judgio/urls.py
@@ -21,7 +21,7 @@ from judging.admin import judge_upload_site
 urlpatterns = [
     path('admin/', admin.site.urls),
     # custom admin site
-    path('admin/judging/judge-csv-upload', judge_upload_site.urls),
+    path('admin/judging/judge-csv-upload/', judge_upload_site.urls),
     # login endpoint
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
