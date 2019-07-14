0a36a2cda74bf84500608fb20dc49c733a12ac37
wukelly <kelly.wu.98@gmail.com>
2019-07-14 16:53:15 -0700

    adding delete user on delete judge and judgeQuerySet, adding model tests

--- judging/models.py
+++ judging/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# makes sure to delete users when deleting judges in bulk
+class JudgeQuerySet(models.QuerySet):
+
+    def delete(self, *args, **kwargs):
+        for obj in self:
+            obj.user.delete()
+        super(JudgeQuerySet, self).delete(*args, **kwargs)
+
 # Add organization and job title atributes to users who are Judges
 class Judge(models.Model):
+    objects = JudgeQuerySet.as_manager()
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -19,9 +29,12 @@ class Judge(models.Model):
     def username(self):
         return self.user.username
 
+    def delete(self, *args, **kwargs):
+        self.user.delete()
+        return super(self.__class__, self).delete(*args, **kwargs)
+
     def __str__(self):
         return self.name()
-
 # make migration commands
 # python manage.py makemigrations judging
 # python manage.py sqlmigrate judging 0001
--- judging/tests/tests_models.py
+++ judging/tests/tests_models.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from judging.models import Judge
+from judging.models import Judge, JudgeQuerySet
 
-# Judge Model Tests
+# Judge and JudgeQuerySet Tests
 class JudgeTestCase(TestCase):
     def setUp(self):
+        # given
         user = User.objects.create_user("username", "email@email.com", 'password')
         self.judge = Judge.objects.create(user = user,
                                           first_name = "First",
@@ -16,15 +17,44 @@ class JudgeTestCase(TestCase):
                                           )
 
     def test_boolean_defaults(self):
-        self.assertEqual(self.judge.sponsor_judge, False)
-        self.assertEqual(self.judge.checked_in, False)
-        self.assertEqual(self.judge.active, True)
+        # then
+        self.assertFalse(self.judge.sponsor_judge)
+        self.assertFalse(self.judge.checked_in)
+        self.assertTrue(self.judge.active)
 
     def test_name(self):
+        # then
         self.assertEqual(self.judge.name(), "First Last")
 
     def test_username(self):
+        # then
         self.assertEqual(self.judge.username(), "username")
 
     def test_str(self):
+        # then
         self.assertEqual(self.judge.__str__(), "First Last")
+
+    def test_delete(self):
+        # when
+        self.judge.delete()
+
+        # then
+        self.assertFalse(self.judge in Judge.objects.all())
+        self.assertFalse(self.judge.user in User.objects.all())
+
+    def test_delete_queryset(self):
+        # when
+        user = User.objects.create_user("username2", "email2@email.com", 'password2')
+        judge = Judge.objects.create(user = user,
+                                     first_name = "Last",
+                                     last_name = "First",
+                                     email = "email2@email.com",
+                                     organization = "Department",
+                                     job_title = "CEO")
+        judges = Judge.objects.all()
+        judges.delete()
+
+        # then
+        for judge in judges:
+            self.assertFalse(judge in Judge.objects.all())
+            self.assertFalse(judge.user in User.objects.all())
